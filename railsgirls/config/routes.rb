1c12cb11782858c3b522a98e60c588ada4c11e14
tomoya-tsuji <tomoya-tsuji@cookpad.com>
2016-08-08 19:19:14 +0900

    Add comment function to idea show page

--- railsgirls/config/routes.rb
+++ railsgirls/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :comments
+  get 'pages/info'
+
   root to: redirect('/ideas')
   resources :ideas
   # The priority is based upon order of creation: first created -> highest priority.
